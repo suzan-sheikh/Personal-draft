840181cc4fdb7c755e043100c686332940d2590b
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-06 23:10:56 +0600

    complite task 1

---
+++ draft.ts
@@ -0,0 +1,32 @@
+// // Problem_1 :
+// //*TODO
+// //? create a function - complete
+// //? This function received only array of number - complete
+// //? This function return a array of number only even number
+
+// // type TInput = number[];
+// // const filterEvenNumbers = (input: TInput): TInput => {
+// //   let output: TInput = [];
+// //   input.map((i) => {
+// //     if (i % 2 == 0) {
+// //       output.push(i);
+// //     }
+// //   });
+// //   return output
+// // };
+// // console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// // type TInput = number[];
+
+// // const filterEvenNumbers = (input: TInput): TInput => {
+// //   return input.filter((i) => i % 2 === 0);
+// // };
+
+// // console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// assignment_1 
+const filterEvenNumbers = (input: number[]): number[] => {
+  return input.filter((i) => i % 2 === 0);
+};
+
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
